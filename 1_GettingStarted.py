28201eb1ef2774974442b0043a7036c66509a2a3
rsatwik <satwikrv@gmail.com>
2019-12-30 17:15:08 +0530

    Introduction and basic plotting

---
+++ 1_GettingStarted.py
@@ -0,0 +1,13 @@
+# starting ipython
+# cmd->ipython3
+
+# exiting ipython
+# ctrl + D
+
+# Printing inside ipython terminal
+print(1+2)
+
+# tab completion is possible in ipython
+
+# for using help for a function add ? at the end of the function name
+abs?

1ebd3890ca25468dac2e1026a1af07d78a24e279
rsatwik <satwikrv@gmail.com>
2020-03-21 21:08:21 +0530

    Created notebook for practice

--- 2_Plots.py
+++
@@ -1,8 +0,0 @@
-%pylab # To intialize the pylab package
-linspace? # help for linspace command
-t=linspace(-pi,pi,100) # (start, end, number of elements) 100 numbers between -pi and pi
-len(t) # returns 100
-cosine=cos(t)
-plot(t,cosine)
-clf() # to clear plots and avoid overlapping of plots in the same figure
-plot(t,sin(t)*sin(t)/t)
